e00aa61eacccf131dbd5d50ab868c1bb2f147514
Tvrtko <tvrtkom@gmail.com>
2016-01-13 20:16:17 +0100

    comment code

--- es2015-src/jqtipnav.es6.js
+++ es2015-src/jqtipnav.es6.js
@@ -7,6 +7,7 @@
 })(function ($) {
   var jQTipnav, _module,
     unique = (function () {
+      // returns incremented numbers
       var id;
       return {
         generate: function () {
@@ -33,6 +34,7 @@
     }
 
     get_geometry(container) {
+      // returns container's and action element's dimensions and offset point
       return {
         'tn': {
           'off': container.offset(),
@@ -48,6 +50,7 @@
     }
 
     get_container_position(container) {
+      // return position of the navigation container on the screen
       var geometry = this.get_geometry(container),
         ac_mid = geometry.ac.off.left + geometry.ac.width/2,
         top = geometry.ac.off.top - geometry.tn.height,
@@ -68,7 +71,10 @@
     }
 
     bind_container(container) {
+      // bind events to container
       var set_hide_timeout = () => {
+        // stores timeout to element's data attribute after
+        // container fades out after timeout of 200
           container.data('hoverTimeout', setTimeout(() => {
             container.fadeOut(this.fade_time || 200, () => {
               container.data('open', false);
@@ -77,10 +83,15 @@
         }
         ;
       this.$el.bind(this.trigger_event, (e) => {
+        // on specified trigger event container should appear
+
         e.preventDefault();
+        // clear hover timeout each time element is triggered
         if (typeof container.data('hoverTimeout') !== 'undefined')
           clearTimeout(container.data('hoverTimeout'));
+
         if(this.trigger_event === 'click') {
+          // start timeout if container visible and only on 'click'
           if(container.data('open') === true) {
             set_hide_timeout();
           }
@@ -88,6 +99,7 @@
         container.show().data('open', true);
       });
       if(this.trigger_event !== 'click') {
+        // runs only on 'click'
         this.$el.mouseleave(function() {
           set_hide_timeout();
         });
@@ -102,6 +114,7 @@
     }
 
     setup_container(container, wrap) {
+      // sets up container with additional events
       var css,
         arrows = container.find('.jqtipnav-prev, .jqtipnav-next')
         ;
@@ -128,6 +141,7 @@
         });
       }
       this.$el.bind(this.trigger_event, (e) => {
+        // on trigger event calculate and set position of container
         if(!wrap) {
           css = this.get_container_position(container);
           container.show().css(css);
@@ -151,12 +165,16 @@
         ;
       this.action = new jQTipnav.Action($action_el, options);
       this.container = jQTipnav.Container.build($copy, wrap);
+
+      // define some properties
       Object.defineProperty(this, '$wrap', {
         'writable': false,
         'value': this.container.$wrap
       });
+
       Object.defineProperty(this, '$container', {
         set: (value) => {
+          // setting property removes original elemnet's parent
           var els = value.children().clone();
           this.container.$el.find('ul').append(els);
         },
@@ -168,6 +186,9 @@
         'writable': false,
         'value': this.container.id
       });
+
+      // TODO: merge setup_container into bind_container and refactor code below
+      // it should be just this.action.bind_container
       if(wrap === true) {
         this.action.bind_container(this.$wrap);
       } else if(!wrap) {
@@ -177,6 +198,8 @@
     }
 
     static build($copy, wrap=false) {
+      // build a container element markup and return it
+      // adds $copy contents and arrows if content should be wrapped (multiple matched)
       var $el, $wrap;
       if(typeof wrap === 'boolean' && wrap === true) {
         $wrap = $('<div class="jqtipnav-wrap"></div>');
@@ -188,11 +211,13 @@
         $wrap.hide();
       }
       else {
+        // assign unique id to each container
         let id = unique.generate();
         $el = $('<div data-jqtipnav=' + id + '><ul></ul></div>');
         $el.find('ul').append($copy.children().clone());
         $copy.attr('data-jqtipnav-main', id);
         if(!!wrap) {
+          // if wrapped add arrow buttons
           let arrow_p = $('<li><a class="jqtipnav-prev" href="#">&lt;</a></li>'),
             arrow_n = $('<li><a class="jqtipnav-next" href="#">&gt;</a></li>')
             ;
@@ -214,21 +239,28 @@
       };
     }
   });
+
   $.fn.tipnav = function (options) {
+    // plugin definition
     var matchedEl = this,
      action,
      multi = matchedEl.length > 1,
      actions = {
        'clean': function () {
+         // can be called on element - $jqtipnav.tipnav('clean')
+         // or on whole document $(document).tipnav('clean')
+         // removes all the mess plugin has done under matched element
          var id, wrap_el, el;
          if (matchedEl.length === 1 && matchedEl.prop('nodeName').toLowerCase() === 'body'
           || matchedEl.prop('nodeName') === '#document') {
+           // if matched element is document or body
            $('body').find('[data-jqtipnav]').remove();
            $('body').find('.jqtipnav-wrap').remove();
            $('[data-jqtipnav-main]').show().removeAttr('data-jqtipnav-main');
          }
          else {
            matchedEl.each(function () {
+             // else remove all jqtpnav mess for only matched elements
              id = parseInt($(this).attr('data-jqtipnav-main'));
              if (typeof id !== 'undefined') {
                el = $('body').find('[data-jqtipnav=' + id + ']');
