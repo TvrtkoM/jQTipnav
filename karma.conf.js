d9784e129f17a9d7ea0f8247b615b03cb6a2c47a
Tvrtko <tvrtkom@gmail.com>
2016-02-02 18:22:46 +0100

    karma - reconfigure + update

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,12 @@
 module.exports = function(config) {
     config.set({
+        autoWatch: false,
         basePath: '',
         'frameworks': ["jasmine"],
         'files': [
-            {pattern: 'vendor/jquery/dist/jquery.js', watched: true, served: true, included: true},
+            {pattern: 'vendor/jquery/jquery.js', watched: true, served: true, included: true},
             {pattern: 'node_modules/jasmine-jquery/lib/jasmine-jquery.js', watched: true, served: true, included: true},
-            {pattern: 'vendor/lodash.js', watched: true, served: true, included: true},
+            {pattern: 'vendor/lodash/lodash.js', watched: true, served: true, included: true},
             {pattern: 'js-compiled/jqtipnav.es6-compiled.js', watched: true, served: true, included: true},
             {pattern: 'spec.html', watched: true, served: true, included: true},
             {pattern: 'spec.js', watched: true, served: true, included: true}
